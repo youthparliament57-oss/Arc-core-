b87f3e7eed2cad68be5cbe99977b19537787c60f
roshan yadav <youthparliament57@gmail.com>
2026-09-20 15:30:45 +0530

    build: update application ID
    Change application ID to com.roshan.ar.view to reflect project branding.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.example"
+    applicationId = "com.roshan.ar.view"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
